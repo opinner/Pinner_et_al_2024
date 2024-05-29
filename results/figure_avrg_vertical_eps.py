6a130ae80f60317dbf69fdfb77e08f21883c1458
opinner <32843689+opinner@users.noreply.github.com>
2024-05-29 12:20:47 +0200

    Added errors to avrg vertical eps plot

--- results/figure_avrg_vertical_eps.py
+++ results/figure_avrg_vertical_eps.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.colors as mcolors
 import pandas as pd
-import scipy.stats as ss
-from matplotlib.markers import MarkerStyle
 
 # read Thorpe and strain results data
 #-------------------------------------------------------------------
@@ -27,29 +24,38 @@ strain_eps = strain_eps*2.694 #Correction from Rw =3 to Rw = 7
 #Horizontally average of the mooring results
 #-------------------------------------------------------------------
 energy_levels = pd.read_csv("../scripts/IDEMIX_parametrization/method_data/eps_IGW_IDEMIX_results.csv")
-mab_ranges = [(20,60),(120,160),(320,400)]
+mab_ranges = [(20, 60),(120, 160),(320, 400)]
 mabs = []
 IGWs = []
+IGW_errors = []
 for mab_range in mab_ranges:
     inbetween_mabs = []
     inbetween_IGWs = []
-    for mab, IGW in zip(energy_levels["rounded_mab"], energy_levels["eps_IGW"]):
+    inbetween_IGW_errors = []
+    for mab, IGW, IGW_error in zip(energy_levels["rounded_mab"], energy_levels["eps_IGW"], energy_levels["eps_IGW_error"]):
         if not (mab > mab_range[0] and mab < mab_range[1]):
             continue
         inbetween_mabs.append(mab)        
-        inbetween_IGWs.append(IGW) 
+        inbetween_IGWs.append(IGW)
+        inbetween_IGW_errors.append(IGW_error)
     mabs.append(np.mean(inbetween_mabs))
     IGWs.append(np.mean(inbetween_IGWs))
-    
-    
+    IGW_errors.append(np.mean(inbetween_IGW_errors))
+
 cm = 1/2.54  # centimeters in inches
 ONE_COLUMN_WIDTH = 8.3
 GOLDEN_RATIO = 1.61
 fig, ax = plt.subplots(1, figsize=(ONE_COLUMN_WIDTH*cm, GOLDEN_RATIO*ONE_COLUMN_WIDTH*cm))
 
 ax.semilogx(thorpe_eps, thorpe_z, c = "tab:red", label = "$\\langle\\varepsilon_{\\mathrm{total, Thorpe}}\\rangle$")
-ax.semilogx(IGWs, mabs, "o", c = "k", alpha = 0.6, label = "$\\langle\\varepsilon_{\\mathrm{IGW, IDEMIX}}\\rangle$", zorder = 10)    
-ax.semilogx(strain_eps, strain_z, linestyle='-', marker='.', c = "tab:blue", label = "$\\langle\\varepsilon_{\\mathrm{IGW, strain}}\\rangle$")
+ax.semilogx(IGWs, mabs, "o", c = "k", label = "$\\langle\\varepsilon_{\\mathrm{IGW, IDEMIX}}\\rangle$", zorder = 10)
+ax.semilogx(strain_eps, strain_z, '-o', lw = 3, ms = 6, c = "tab:blue", mec = "xkcd:darkblue", label = "$\\langle\\varepsilon_{\\mathrm{IGW, strain}}\\rangle$")
+
+# Plot Errors
+for IGW, IGW_error, mab in zip(IGWs, IGW_errors, mabs):
+    ax.plot([IGW-IGW_error, IGW+IGW_error], [mab,mab], lw = 3, c = "k", alpha = 0.5)
+ax.fill_betweenx(thorpe_z, thorpe_eps/5, thorpe_eps*5, color = "tab:red", alpha = 0.5)
+ax.fill_betweenx(strain_z, strain_eps/5, strain_eps*5, color = "tab:blue", alpha = 0.3)
 
 
 ax.set_ylabel("Distance from sea floor (m)")
