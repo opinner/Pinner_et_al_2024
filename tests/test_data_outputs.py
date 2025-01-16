035cc9bdc820930f789ebc1e105643861dff0f80
opinner <32843689+opinner@users.noreply.github.com>
2025-01-16 01:50:28 +0100

    bugfix test

--- tests/test_data_outputs.py
+++ tests/test_data_outputs.py
@@ -6,7 +6,6 @@ def test_thorpe_dissipation_rate_output(dataframe_regression):
     thorpe_eps_df = pd.read_pickle("./scripts/thorpe_scales/method_results/Thorpe_eps_df_with_mab.pkl")
     dataframe_regression.check(thorpe_eps_df, default_tolerance=dict(rtol=1e-05, atol=1e-10))
 
-
 def test_finestructure_dissipation_rate_output(dataframe_regression):
     eps_IGW_strain_df = pd.read_csv("./scripts/finestructure/method_results/binned_strain_eps.csv")
     dataframe_regression.check(eps_IGW_strain_df, default_tolerance=dict(rtol=1e-05, atol=1e-10))
